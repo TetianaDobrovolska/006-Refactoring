7cf059e2a903cc97449eeb0a587dc7e526eff179
Thomas Wursthorn <Thomas.Wursthorn@partner.bmw.de>
2021-03-22 23:28:41 +0100

    step1

--- src/string_calc.cpp
+++ src/string_calc.cpp
@@ -16,5 +16,30 @@ StringCalc::~StringCalc()
 
 int StringCalc::Add(string numbers)
 {
-	return 0;//Not Implemented yet
+	uint8_t sum = 0;
+	if (numbers != "")
+	{
+		if (numbers.find_first_not_of("012,") == string::npos)
+		{
+			string delimiter = ",";
+			auto deliPosition = numbers.find(delimiter);
+			if (deliPosition == string::npos)
+			{
+				sum = stoi(numbers);
+			}
+			else
+			{
+				const uint8_t a = stoi(numbers.substr(0, 1));
+				const uint8_t b = stoi(numbers.substr(deliPosition+1, 1));
+				sum = a + b;
+			}
+		}
+		else
+		{
+			return -1;
+		}
+	}
+	
+
+	return sum;
 }
--- test/main_tests/test.cpp
+++ test/main_tests/test.cpp
@@ -2,14 +2,27 @@
 #include "string_calc.hpp"
 #include <iostream>
 
-TEST(CalculatorTest, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+TEST(CalculatorTest, emptyString) {
+    StringCalc c;
+    int actual = c.Add("");
+    ASSERT_EQ(actual, 0);
 }
 
-// TEST(CalculatorTest, SampleTest) {
-// 	StringCalc c;
-// 	int actual = c.Add("2,2");
-// 	ASSERT_EQ(actual, 4);
-// }
+ TEST(CalculatorTest, oneNumber) {
+ 	StringCalc c;
+ 	int actual = c.Add("2");
+ 	ASSERT_EQ(actual, 2);
+ }
+
+ TEST(CalculatorTest, twoNumber) {
+     StringCalc c;
+     int actual = c.Add("1,2");
+     ASSERT_EQ(actual, 3);
+ }
+
+ TEST(CalculatorTest, twoNumber) {
+     StringCalc c;
+     int actual = c.Add("1.2");
+     ASSERT_EQ(actual, -1);
+ }
 

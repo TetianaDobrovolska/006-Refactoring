669236437269520090bc6600c092910d61cf4398
Lorenzo Mignanelli <LMignanelli@luxoft.com>
2021-03-25 22:46:21 +0100

    test: add more tests
    Signed-off-by: Lorenzo Mignanelli <LMignanelli@luxoft.com>

--- test/main_tests/test.cpp
+++ test/main_tests/test.cpp
@@ -54,6 +54,34 @@ TEST(LAB2, PlayerBuyNoOwnedCompanies)
     ASSERT_TRUE(get<2>(x) != 0);
 }
 
+TEST(LAB2, PlayerCannotBuyTwice)
+{
+    string players[]{ "Peter","Ekaterina","Alexander" };
+
+    Monopoly monopoly(players, 3);
+
+    auto x = monopoly.GetFieldByName("Ford");
+    monopoly.Buy(1, x);
+
+    x = monopoly.GetFieldByName("Ford");
+    bool ret = monopoly.Buy(1, x);
+    ASSERT_FALSE(ret);
+}
+
+TEST(LAB2, PlayerCannotBuyIfIsOwned)
+{
+    string players[]{ "Peter","Ekaterina","Alexander" };
+
+    Monopoly monopoly(players, 3);
+
+    auto x = monopoly.GetFieldByName("Ford");
+    monopoly.Buy(1, x);
+
+    x = monopoly.GetFieldByName("Ford");
+    bool ret = monopoly.Buy(2, x);
+    ASSERT_FALSE(ret);
+}
+
 TEST(LAB2, RentaShouldBeCorrectTransferMoney)
 {
     string players[]{ "Peter","Ekaterina","Alexander" };
@@ -70,6 +98,46 @@ TEST(LAB2, RentaShouldBeCorrectTransferMoney)
     ASSERT_EQ(get<1>(player2), 5750);
 }
 
+// fails since there is no logic to prevent the same player from renting twice
+//TEST(LAB2, PlayerCannotRentTwice)
+//{
+//    string players[]{ "Peter","Ekaterina","Alexander" };
+//    Monopoly monopoly(players, 3);
+//    auto x = monopoly.GetFieldByName("Ford");
+//    monopoly.Buy(1, x);
+
+//    x = monopoly.GetFieldByName("Ford");
+//    monopoly.Renta(2, x);
+
+//    x = monopoly.GetFieldByName("Ford");
+//    bool ret = monopoly.Renta(2, x);
+//    ASSERT_FALSE(ret);
+//}
+
+TEST(LAB2, PayBailToLeavePrison)
+{
+    string players[]{ "Peter","Ekaterina","Alexander" };
+    Monopoly monopoly(players, 3);
+
+    auto x = monopoly.GetFieldByName("Prison");
+    monopoly.Renta(1, x);
+    auto player1 = monopoly.GetPlayerInfo(1);
+    ASSERT_EQ(get<1>(player1), 5000);
+}
+
+// fails since there is no logic to prevent the money to go negative and be used again
+//TEST(LAB2, CannotRentWithNegativeMoney)
+//{
+//    string players[]{ "Peter","Ekaterina","Alexander" };
+//    Monopoly monopoly(players, 3);
+
+//    auto x = monopoly.GetFieldByName("Prison");
+//    bool ret;
+//    for (int i=0; i<6; i++) {
+//        ret = monopoly.Renta(1, x);
+//    }
+//    ASSERT_FALSE(ret);
+//}
 
 bool operator== (std::tuple<std::string, Monopoly::Type, int, bool> & a , std::tuple<std::string, Monopoly::Type, int, bool> & b)
 {

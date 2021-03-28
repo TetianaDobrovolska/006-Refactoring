82c69ae252e281d9a4050402f0cac7366a15cf1e
Maxim Shvyndya <mshvyndya@luxoft.com>
2021-03-28 19:45:28 +0200

    Add a test

--- test/main_tests/test.cpp
+++ test/main_tests/test.cpp
@@ -1,8 +1,16 @@
 #include "gtest/gtest.h"
+#include "tictactoe.h"
 
 using namespace ::std;
 
 TEST(TestSet, Test) {
 
     ASSERT_TRUE(1);
+}
+
+TEST(TestSet, checkInitial) {
+    TicTacToe game;
+    auto expected = '-';
+    auto actual = game.check();
+    ASSERT_EQ(actual, expected);
 }

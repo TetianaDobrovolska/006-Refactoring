76ece435796c61de68064bb5e24db9dd46dc8461
twursthorn <twursthorn@luxoft.com>
2021-03-29 08:44:19 +0200

    replacing magic numbers

--- monolopy.cpp
+++ monolopy.cpp
@@ -3,28 +3,28 @@
 
 using namespace ::std;
 
-Monopoly::Monopoly(string names[10],int countPlaers)
+Monopoly::Monopoly(string names[MAX_PLAYER], int countPlaers)
 {
 	for (int i = 0; i < countPlaers; i++)
 	{
 		Players.push_back(make_tuple(names[i], 6000));
 	}
-	Fields.push_back(make_tuple("Ford", Monopoly::AUTO, 0, false));
-	Fields.push_back(make_tuple("MCDonald", Monopoly::FOOD, 0, false));
-	Fields.push_back(make_tuple("Lamoda", Monopoly::CLOTHER, 0, false));
-	Fields.push_back(make_tuple("Air Baltic", Monopoly::TRAVEL, 0, false));
-	Fields.push_back(make_tuple("Nordavia", Monopoly::TRAVEL, 0, false));
-	Fields.push_back(make_tuple("Prison", Monopoly::PRISON, 0, false));
-	Fields.push_back(make_tuple("MCDonald", Monopoly::FOOD, 0, false));
-	Fields.push_back(make_tuple("TESLA", Monopoly::AUTO, 0, false));
+	Fields.push_back(make_tuple("Ford", Monopoly::AUTO, AVAILABLE, false));
+	Fields.push_back(make_tuple("MCDonald", Monopoly::FOOD, AVAILABLE, false));
+	Fields.push_back(make_tuple("Lamoda", Monopoly::CLOTHER, AVAILABLE, false));
+	Fields.push_back(make_tuple("Air Baltic", Monopoly::TRAVEL, AVAILABLE, false));
+	Fields.push_back(make_tuple("Nordavia", Monopoly::TRAVEL, AVAILABLE, false));
+	Fields.push_back(make_tuple("Prison", Monopoly::PRISON, AVAILABLE, false));
+	Fields.push_back(make_tuple("MCDonald", Monopoly::FOOD, AVAILABLE, false));
+	Fields.push_back(make_tuple("TESLA", Monopoly::AUTO, AVAILABLE, false));
 }
 
-std::list<std::tuple<std::string, int>> * Monopoly::GetPlayersList()
+std::list<std::tuple<std::string, int>>* Monopoly::GetPlayersList()
 {
 	return &Players;
 }
 
-std::list<std::tuple<std::string, Monopoly::Type,int,bool>> * Monopoly::GetFieldsList()
+std::list<std::tuple<std::string, Monopoly::Type, int, bool>>* Monopoly::GetFieldsList()
 {
 	return &Fields;
 }
@@ -42,47 +42,47 @@ bool Monopoly::Buy(int z, std::tuple<std::string, Type, int, bool> k)
 	tuple<string, int> p;
 	list<tuple<std::string, Type, int, bool>>::iterator i;
 	list<tuple<string, int>>::iterator j = Players.begin();
-	switch (get<1>(k))
+	switch (get<FIELD_TYPE_INDEX>(k))
 	{
 	case AUTO:
-		if (get<2>(k))
+		if (get<FIELD_OWNERSHIP_INDEX>(k))
 			return false;
-		p = make_tuple(get<0>(x), get<1>(x) - 500);
-		k = make_tuple(get<0>(k), get<1>(k), z, get<2>(k));
+		p = make_tuple(get<PLAYER_NAME_INDEX>(x), get<PLAYER_MONEY_INDEX>(x) - AUTO_BUY_PRICE);
+		k = make_tuple(get<FIELD_NAME_INDEX>(k), get<FIELD_TYPE_INDEX>(k), z, get<FIELD_OWNERSHIP_INDEX>(k));
 		break;
 	case FOOD:
-		if (get<2>(k))
+		if (get<FIELD_OWNERSHIP_INDEX>(k))
 			return false;
-		p = make_tuple(get<0>(x), get<1>(x) - 250);
-		k = make_tuple(get<0>(k), get<1>(k), z, get<2>(k));
+		p = make_tuple(get<PLAYER_NAME_INDEX>(x), get<PLAYER_MONEY_INDEX>(x) - FOOD_BUY_PRICE);
+		k = make_tuple(get<FIELD_NAME_INDEX>(k), get<FIELD_TYPE_INDEX>(k), z, get<FIELD_OWNERSHIP_INDEX>(k));
 		break;
 	case TRAVEL:
-		if (get<2>(k))
+		if (get<FIELD_OWNERSHIP_INDEX>(k))
 			return false;
-		p = make_tuple(get<0>(x), get<1>(x) - 700);
-		k = make_tuple(get<0>(k), get<1>(k), z, get<2>(k));
+		p = make_tuple(get<PLAYER_NAME_INDEX>(x), get<PLAYER_MONEY_INDEX>(x) - TRAVEL_BUY_PRICE);
+		k = make_tuple(get<FIELD_NAME_INDEX>(k), get<FIELD_TYPE_INDEX>(k), z, get<FIELD_OWNERSHIP_INDEX>(k));
 		break;
 	case CLOTHER:
-		if (get<2>(k))
+		if (get<FIELD_OWNERSHIP_INDEX>(k))
 			return false;
-		p = make_tuple(get<0>(x), get<1>(x) - 100);
-		k = make_tuple(get<0>(k), get<1>(k), z, get<2>(k));
+		p = make_tuple(get<PLAYER_NAME_INDEX>(x), get<PLAYER_MONEY_INDEX>(x) - CLOTHER_BUY_PRICE);
+		k = make_tuple(get<FIELD_NAME_INDEX>(k), get<FIELD_TYPE_INDEX>(k), z, get<FIELD_OWNERSHIP_INDEX>(k));
 		break;
 	default:
 		return false;
 	};
-	i = find_if(Fields.begin(), Fields.end(), [k](auto x) { return get<0>(x) == get<0>(k); });
+	i = find_if(Fields.begin(), Fields.end(), [k](auto x) { return get<PLAYER_NAME_INDEX>(x) == get<FIELD_NAME_INDEX>(k); });
 	*i = k;
-    advance(j, z-1);
+	advance(j, z - 1);
 	*j = p;
 	return true;
 }
 
 std::tuple<std::string, Monopoly::Type, int, bool>  Monopoly::GetFieldByName(std::string l)
 {
-	std::list<std::tuple<std::string, Monopoly::Type, int, bool>>::iterator i = find_if(Fields.begin(), Fields.end(),[l] (std::tuple<std::string, Monopoly::Type, int, bool> x) {
-		return get<0>(x) == l;
-	});
+	std::list<std::tuple<std::string, Monopoly::Type, int, bool>>::iterator i = find_if(Fields.begin(), Fields.end(), [l](std::tuple<std::string, Monopoly::Type, int, bool> x) {
+		return get<FIELD_NAME_INDEX>(x) == l;
+		});
 	return *i;
 }
 
@@ -91,37 +91,37 @@ bool Monopoly::Renta(int m, std::tuple<std::string, Type, int, bool> c)
 	tuple<string, int> z = GetPlayerInfo(m);
 	tuple<string, int> o;
 
-	switch (get<1>(c))
+	switch (get<FIELD_TYPE_INDEX>(c))
 	{
 	case AUTO:
-		if (!get<2>(c))
+		if (!get<FIELD_OWNERSHIP_INDEX>(c))
 			return false;
-		o = GetPlayerInfo(get<2>(c));
-		o = make_tuple(get<0>(o), get<1>(o) + 250);
-		z = make_tuple(get<0>(z), get<1>(z) - 250);
+		o = GetPlayerInfo(get<FIELD_OWNERSHIP_INDEX>(c));
+		o = make_tuple(get<PLAYER_NAME_INDEX>(o), get<PLAYER_MONEY_INDEX>(o) + AUTO_RENTAL_CHARGE);
+		z = make_tuple(get<PLAYER_NAME_INDEX>(z), get<PLAYER_MONEY_INDEX>(z) - AUTO_RENTAL_CHARGE);
 		break;
 	case FOOD:
-		if (!get<2>(c))
+		if (!get<FIELD_OWNERSHIP_INDEX>(c))
 			return false;
 	case TRAVEL:
-		if (!get<2>(c))
+		if (!get<FIELD_OWNERSHIP_INDEX>(c))
 			return false;
-		o = GetPlayerInfo(get<2>(c));
-		o = make_tuple(get<0>(o), get<1>(o) + 250);
-		z = make_tuple(get<0>(z), get<1>(z) - 250);
+		o = GetPlayerInfo(get<FIELD_OWNERSHIP_INDEX>(c));
+		o = make_tuple(get<PLAYER_NAME_INDEX>(o), get<PLAYER_MONEY_INDEX>(o) + TRAVEL_RENTAL_CHARGE);
+		z = make_tuple(get<PLAYER_NAME_INDEX>(z), get<PLAYER_MONEY_INDEX>(z) - TRAVEL_RENTAL_CHARGE);
 		break;
 	case CLOTHER:
-		if (!get<2>(c))
+		if (!get<FIELD_OWNERSHIP_INDEX>(c))
 			return false;
-		o = GetPlayerInfo(get<2>(c));
-		o = make_tuple(get<0>(o), get<1>(o) + 250);
-		z = make_tuple(get<0>(z), get<1>(z) - 250);
+		o = GetPlayerInfo(get<FIELD_OWNERSHIP_INDEX>(c));
+		o = make_tuple(get<PLAYER_NAME_INDEX>(o), get<PLAYER_MONEY_INDEX>(o) + CLOTHER_RENTAL_CHARGE);
+		z = make_tuple(get<PLAYER_NAME_INDEX>(z), get<PLAYER_MONEY_INDEX>(z) - CLOTHER_RENTAL_CHARGE);
 		break;
 	case PRISON:
-		z = make_tuple(get<0>(z), get<1>(z) - 1000);
+		z = make_tuple(get<PLAYER_NAME_INDEX>(z), get<PLAYER_MONEY_INDEX>(z) - PRISON_RENTAL_CHARGE);
 		break;
 	case BANK:
-		z = make_tuple(get<0>(z), get<1>(z) - 700);
+		z = make_tuple(get<PLAYER_NAME_INDEX>(z), get<PLAYER_MONEY_INDEX>(z) - BANK_RENTAL_CHARGE);
 		break;
 	default:
 		return false;
@@ -129,7 +129,7 @@ bool Monopoly::Renta(int m, std::tuple<std::string, Type, int, bool> c)
 	list<tuple<string, int>>::iterator i = Players.begin();
 	advance(i, m - 1);
 	*i = z;
-	i = find_if(Players.begin(), Players.end(), [o](auto x) { return get<0>(x) == get<0>(o); });
+	i = find_if(Players.begin(), Players.end(), [o](auto x) { return get<PLAYER_NAME_INDEX>(x) == get<PLAYER_NAME_INDEX>(o); });
 	*i = o;
 	return true;
 }
--- monolopy.hpp
+++ monolopy.hpp
@@ -17,16 +17,40 @@ public:
 		PRISON,
 		BANK
 	};
-	Monopoly(std::string names[10],int);
-	std::list<std::tuple<std::string,int>> *	GetPlayersList();
-	std::list<std::tuple<std::string, Type, int,bool>> * GetFieldsList();
+	Monopoly(std::string names[MAX_PLAYER], int);
+	std::list<std::tuple<std::string, int>>* GetPlayersList();
+	std::list<std::tuple<std::string, Type, int, bool>>* GetFieldsList();
 	std::list<std::tuple<std::string, Type, int, bool>> Fields;
 	std::list<std::tuple<std::string, int>> Players;
-	std::tuple<std::string, int>  GetPlayerInfo(int);	 
-	bool Buy(int p,std::tuple<std::string, Type, int, bool>);
-	std::tuple<std::string, Type, int, bool> GetFieldByName(std::string);	
+	std::tuple<std::string, int>  GetPlayerInfo(int);
+	bool Buy(int p, std::tuple<std::string, Type, int, bool>);
+	std::tuple<std::string, Type, int, bool> GetFieldByName(std::string);
 	bool Renta(int p, std::tuple<std::string, Type, int, bool>c);
-	
+
+private:
+	static constexpr uint8_t MAX_PLAYER = 10U;
+	static constexpr uint8_t AVAILABLE = 0U;
+
+	static constexpr uint8_t FIELD_NAME_INDEX = 0U;
+	static constexpr uint8_t FIELD_TYPE_INDEX = 1U;
+	static constexpr uint8_t FIELD_OWNERSHIP_INDEX = 2U;
+
+	static constexpr uint8_t PLAYER_NAME_INDEX = 0U;
+	static constexpr uint8_t PLAYER_MONEY_INDEX = 1U;
+
+	static constexpr uint16_t AUTO_BUY_PRICE = 500U;
+	static constexpr uint16_t AUTO_RENTAL_CHARGE = 250U;
+	static constexpr uint16_t FOOD_BUY_PRICE = 250U;
+	static constexpr uint16_t FOOD_RENTAL_CHARGE = 0U;
+	static constexpr uint16_t TRAVEL_BUY_PRICE = 700U;
+	static constexpr uint16_t TRAVEL_RENTAL_CHARGE = 250U;
+	static constexpr uint16_t CLOTHER_BUY_PRICE = 100U;
+	static constexpr uint16_t CLOTHER_RENTAL_CHARGE = 250U;
+	//static constexpr uint16_t PRISON_BUY_PRICE = 0U;
+	static constexpr uint16_t PRISON_RENTAL_CHARGE = 1000U;
+	//static constexpr uint16_t BANK_BUY_PRICE = 0U;
+	static constexpr uint16_t BANK_RENTAL_CHARGE = 700U;
+
 };
 
 #endif

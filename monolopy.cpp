7571be89d415a1393e23e584770f74c4e1ad382f
twursthorn <twursthorn@luxoft.com>
2021-03-30 09:56:17 +0200

    split type into classes

--- monolopy.cpp
+++ monolopy.cpp
@@ -7,130 +7,80 @@ Monopoly::Monopoly(string names[MAX_PLAYER], int countPlaers)
 {
 	for (int i = 0; i < countPlaers; i++)
 	{
-		_players.push_back(make_tuple(names[i], 6000));
+		_players.push_back(Player(names[i]));
 	}
-	_fields.push_back(make_tuple("Ford", FieldType::AUTO, AVAILABLE, false));
-	_fields.push_back(make_tuple("MCDonald", FieldType::FOOD, AVAILABLE, false));
-	_fields.push_back(make_tuple("Lamoda", FieldType::CLOTHER, AVAILABLE, false));
-	_fields.push_back(make_tuple("Air Baltic", FieldType::TRAVEL, AVAILABLE, false));
-	_fields.push_back(make_tuple("Nordavia", FieldType::TRAVEL, AVAILABLE, false));
-	_fields.push_back(make_tuple("Prison", FieldType::PRISON, AVAILABLE, false));
-	_fields.push_back(make_tuple("MCDonald", FieldType::FOOD, AVAILABLE, false));
-	_fields.push_back(make_tuple("TESLA", FieldType::AUTO, AVAILABLE, false));
+
+	_fields.push_back(new Auto("Ford"));
+	_fields.push_back(new Food("MCDonald"));
+	_fields.push_back(new Clother("Lamoda"));
+	_fields.push_back(new Travel("Air Baltic"));
+	_fields.push_back(new Travel("Nordavia"));
+	_fields.push_back(new Prison("Prison"));
+	_fields.push_back(new Food("MCDonald"));
+	_fields.push_back(new Auto("TESLA"));
 }
 
-std::list<std::tuple<std::string, int>>* Monopoly::GetPlayersList()
+Field* Monopoly::GetFieldByName(std::string fieldName) const
+{
+	for (auto element : _fields)
+	{
+		if (element->getFieldName() == fieldName)
+		{
+			return element;
+		}
+	}
+	return nullptr;
+}
+
+Player* Monopoly::GetPlayerByName(std::string playerName) const
+{
+	for (auto element : _players)
+	{
+		if (element.getPlayerName() == playerName)
+		{
+			return &element;
+		}
+	}
+	return nullptr;
+}
+
+const Monopoly::PlayerVector* Monopoly::GetPlayers() const
 {
 	return &_players;
 }
 
-std::list<std::tuple<std::string, Monopoly::FieldType, int, bool>>* Monopoly::GetFieldsList()
-{
-	return &_fields;
-}
 
-Monopoly::PlayerTuple Monopoly::GetPlayerInfo(int playerIndex)
+bool Monopoly::Buy(const std::string playerName, const std::string fieldName)
 {
-	PlayerList::iterator playerIterator = _players.begin();
-	advance(playerIterator, playerIndex - 1);
-	return *playerIterator;
-}
-
-bool Monopoly::Buy(int playerIndex, FieldTuple field)
-{
-	auto player = GetPlayerInfo(playerIndex);
-	tuple<string, int> tempPlayer;
-	FieldList::iterator fieldIterator;
-	PlayerList::iterator playerIterator = _players.begin();
-	switch (get<FIELD_TYPE_INDEX>(field))
-	{
-	case FieldType::AUTO:
-		if (get<FIELD_OWNERSHIP_INDEX>(field))
-			return false;
-		tempPlayer = make_tuple(get<PLAYER_NAME_INDEX>(player), get<PLAYER_MONEY_INDEX>(player) - AUTO_BUY_PRICE);
-		field = make_tuple(get<FIELD_NAME_INDEX>(field), get<FIELD_TYPE_INDEX>(field), playerIndex, get<FIELD_OWNERSHIP_INDEX>(field));
-		break;
-	case FieldType::FOOD:
-		if (get<FIELD_OWNERSHIP_INDEX>(field))
-			return false;
-		tempPlayer = make_tuple(get<PLAYER_NAME_INDEX>(player), get<PLAYER_MONEY_INDEX>(player) - FOOD_BUY_PRICE);
-		field = make_tuple(get<FIELD_NAME_INDEX>(field), get<FIELD_TYPE_INDEX>(field), playerIndex, get<FIELD_OWNERSHIP_INDEX>(field));
-		break;
-	case FieldType::TRAVEL:
-		if (get<FIELD_OWNERSHIP_INDEX>(field))
-			return false;
-		tempPlayer = make_tuple(get<PLAYER_NAME_INDEX>(player), get<PLAYER_MONEY_INDEX>(player) - TRAVEL_BUY_PRICE);
-		field = make_tuple(get<FIELD_NAME_INDEX>(field), get<FIELD_TYPE_INDEX>(field), playerIndex, get<FIELD_OWNERSHIP_INDEX>(field));
-		break;
-	case FieldType::CLOTHER:
-		if (get<FIELD_OWNERSHIP_INDEX>(field))
-			return false;
-		tempPlayer = make_tuple(get<PLAYER_NAME_INDEX>(player), get<PLAYER_MONEY_INDEX>(player) - CLOTHER_BUY_PRICE);
-		field = make_tuple(get<FIELD_NAME_INDEX>(field), get<FIELD_TYPE_INDEX>(field), playerIndex, get<FIELD_OWNERSHIP_INDEX>(field));
-		break;
-	default:
+	Field* field = GetFieldByName(fieldName);
+	if (field == nullptr)
 		return false;
-	};
-	fieldIterator = find_if(_fields.begin(), _fields.end(), [field](auto player) { return get<PLAYER_NAME_INDEX>(player) == get<FIELD_NAME_INDEX>(field); });
-	*fieldIterator = field;
-	advance(playerIterator, playerIndex - 1);
-	*playerIterator = tempPlayer;
+	if (field->getOwnershipName() != "")
+		return false;
+
+	Player* player	    = GetPlayerByName(playerName);
+	uint16_t fieldPrice = field->getBuyingPrice();
+	if (player->getPlayerBalance() < fieldPrice)
+		return false;
+
+	player->changePlayerBalance(fieldPrice);
+	field->changeOwnership(playerName);
+
 	return true;
 }
 
-Monopoly::FieldTuple  Monopoly::GetFieldByName(std::string fieldName)
+bool Monopoly::Renta(const std::string playerName, const Field* field)
 {
-	FieldList::iterator fieldListIterator = find_if(_fields.begin(), _fields.end(), [fieldName](FieldTuple field) {
-		return get<FIELD_NAME_INDEX>(field) == fieldName;
-		});
-	return *fieldListIterator;
-}
+	Player* player = GetPlayerByName(playerName);
 
-bool Monopoly::Renta(int playerIndex, FieldTuple field)
-{
-	tuple<string, int> player = GetPlayerInfo(playerIndex);
-	tuple<string, int> ownerPlayer;
-
-	switch (get<FIELD_TYPE_INDEX>(field))
-	{
-	case FieldType::AUTO:
-		if (!get<FIELD_OWNERSHIP_INDEX>(field))
-			return false;
-		ownerPlayer = GetPlayerInfo(get<FIELD_OWNERSHIP_INDEX>(field));
-		ownerPlayer = make_tuple(get<PLAYER_NAME_INDEX>(ownerPlayer), get<PLAYER_MONEY_INDEX>(ownerPlayer) + AUTO_RENTAL_CHARGE);
-		player = make_tuple(get<PLAYER_NAME_INDEX>(player), get<PLAYER_MONEY_INDEX>(player) - AUTO_RENTAL_CHARGE);
-		break;
-	case FieldType::FOOD:
-		if (!get<FIELD_OWNERSHIP_INDEX>(field))
-			return false;
-	case FieldType::TRAVEL:
-		if (!get<FIELD_OWNERSHIP_INDEX>(field))
-			return false;
-		ownerPlayer = GetPlayerInfo(get<FIELD_OWNERSHIP_INDEX>(field));
-		ownerPlayer = make_tuple(get<PLAYER_NAME_INDEX>(ownerPlayer), get<PLAYER_MONEY_INDEX>(ownerPlayer) + TRAVEL_RENTAL_CHARGE);
-		player = make_tuple(get<PLAYER_NAME_INDEX>(player), get<PLAYER_MONEY_INDEX>(player) - TRAVEL_RENTAL_CHARGE);
-		break;
-	case FieldType::CLOTHER:
-		if (!get<FIELD_OWNERSHIP_INDEX>(field))
-			return false;
-		ownerPlayer = GetPlayerInfo(get<FIELD_OWNERSHIP_INDEX>(field));
-		ownerPlayer = make_tuple(get<PLAYER_NAME_INDEX>(ownerPlayer), get<PLAYER_MONEY_INDEX>(ownerPlayer) + CLOTHER_RENTAL_CHARGE);
-		player = make_tuple(get<PLAYER_NAME_INDEX>(player), get<PLAYER_MONEY_INDEX>(player) - CLOTHER_RENTAL_CHARGE);
-		break;
-	case FieldType::PRISON:
-		player = make_tuple(get<PLAYER_NAME_INDEX>(player), get<PLAYER_MONEY_INDEX>(player) - PRISON_RENTAL_CHARGE);
-		break;
-	case FieldType::BANK:
-		player = make_tuple(get<PLAYER_NAME_INDEX>(player), get<PLAYER_MONEY_INDEX>(player) - BANK_RENTAL_CHARGE);
-		break;
-	default:
+	std::string ownerName = field->getOwnershipName();
+	if (ownerName == "")
 		return false;
-	}
-	PlayerList::iterator playerIterator = _players.begin();
-	advance(playerIterator, playerIndex - 1);
-	*playerIterator = player;
-	playerIterator = find_if(_players.begin(), _players.end(), [ownerPlayer](auto player) { return get<PLAYER_NAME_INDEX>(x) == get<PLAYER_NAME_INDEX>(ownerPlayer); });
-	*playerIterator = ownerPlayer;
+	Player* owner = GetPlayerByName(ownerName);
+	const uint16_t rentalCost = field->getRentalCost();
+	player->changePlayerBalance(-rentalCost);
+	owner->changePlayerBalance(rentalCost);
+
 	return true;
 }
 
--- monolopy.hpp
+++ monolopy.hpp
@@ -5,58 +5,157 @@
 #include <tuple>
 #include <string>
 
+static constexpr uint8_t MAX_PLAYER = 10U;
+
+static constexpr uint16_t PLAYER_STARTING_MONEY = 6000U;
+
+static constexpr uint16_t AUTO_BUY_PRICE = 500U;
+static constexpr uint16_t AUTO_RENTAL_CHARGE = 250U;
+static constexpr uint16_t FOOD_BUY_PRICE = 250U;
+static constexpr uint16_t FOOD_RENTAL_CHARGE = 0U;
+static constexpr uint16_t TRAVEL_BUY_PRICE = 700U;
+static constexpr uint16_t TRAVEL_RENTAL_CHARGE = 250U;
+static constexpr uint16_t CLOTHER_BUY_PRICE = 100U;
+static constexpr uint16_t CLOTHER_RENTAL_CHARGE = 250U;
+static constexpr uint16_t PRISON_BUY_PRICE = std::numeric_limits<uint16_t>::max();
+static constexpr uint16_t PRISON_RENTAL_CHARGE = 1000U;
+static constexpr uint16_t BANK_BUY_PRICE = std::numeric_limits<uint16_t>::max();
+static constexpr uint16_t BANK_RENTAL_CHARGE = 700U;
+
+class Player;
+class Field;
+
 class Monopoly
 {
-private:
-	static constexpr uint8_t MAX_PLAYER = 10U;
-	static constexpr uint8_t AVAILABLE = 0U;
-
-	static constexpr uint8_t FIELD_NAME_INDEX = 0U;
-	static constexpr uint8_t FIELD_TYPE_INDEX = 1U;
-	static constexpr uint8_t FIELD_OWNERSHIP_INDEX = 2U;
-
-	static constexpr uint8_t PLAYER_NAME_INDEX = 0U;
-	static constexpr uint8_t PLAYER_MONEY_INDEX = 1U;
-
-	static constexpr uint16_t AUTO_BUY_PRICE = 500U;
-	static constexpr uint16_t AUTO_RENTAL_CHARGE = 250U;
-	static constexpr uint16_t FOOD_BUY_PRICE = 250U;
-	static constexpr uint16_t FOOD_RENTAL_CHARGE = 0U;
-	static constexpr uint16_t TRAVEL_BUY_PRICE = 700U;
-	static constexpr uint16_t TRAVEL_RENTAL_CHARGE = 250U;
-	static constexpr uint16_t CLOTHER_BUY_PRICE = 100U;
-	static constexpr uint16_t CLOTHER_RENTAL_CHARGE = 250U;
-	//static constexpr uint16_t PRISON_BUY_PRICE = 0U;
-	static constexpr uint16_t PRISON_RENTAL_CHARGE = 1000U;
-	//static constexpr uint16_t BANK_BUY_PRICE = 0U;
-	static constexpr uint16_t BANK_RENTAL_CHARGE = 700U;
-
 public:
-	enum class FieldType : uint8_t
-	{
-		AUTO,
-		FOOD,
-		TRAVEL,
-		CLOTHER,
-		PRISON,
-		BANK
-	};
-	using FieldTuple = std::tuple<std::string, Monopoly::FieldType, int, bool>;
-	using PlayerTuple = std::tuple<std::string, int>;
-	using PlayerList = std::list<PlayerTuple>;
-	using FieldList = std::list<FieldTuple>;
+	using PlayerVector = std::vector<Player>;
+	using FieldList = std::list<Field*>;
 
 	Monopoly(std::string names[MAX_PLAYER], int);
-	bool Buy(int playerIndex, FieldTuple field);
-	bool Renta(int playerIndex, FieldTuple field);
-	PlayerList* GetPlayersList();
-	FieldList* GetFieldsList();
-	FieldTuple GetFieldByName(std::string);
-	PlayerTuple  GetPlayerInfo(int);
 
+	bool Renta(const std::string playerName, const Field* field);
+	const PlayerVector* GetPlayers() const;
+	bool Buy(const std::string playerName, const std::string fieldName);
 private:
+	Field* GetFieldByName(std::string fieldName) const;
+	Player* GetPlayerByName(std::string playerName) const;
 
 	FieldList _fields;
-	PlayerList _players;
+	PlayerVector _players;
+};
+
+class Player
+{
+public:
+	Player(const std::string playerName) :
+		_name(playerName),
+		_balance(PLAYER_STARTING_MONEY)
+	{
+	}
+	std::string getPlayerName() const { return _name; };
+	uint16_t getPlayerBalance() const { return _balance; };
+	void changePlayerBalance(uint16_t value) { _balance += value; };
+private:
+	const std::string _name;
+	uint16_t _balance;
+};
+
+class Field
+{
+public:
+	Field(const std::string fieldName) :
+		_fieldName(fieldName),
+		_ownershipName("")
+	{};
+
+	virtual std::string getFieldName() const {return _fieldName;};
+	virtual std::string getOwnershipName() const { return _ownershipName; };
+	virtual void changeOwnership(std::string playerName) { _ownershipName = playerName; };
+	virtual uint16_t getBuyingPrice() const = 0;
+	virtual uint16_t getRentalCost() const = 0;
+
+private:
+	const std::string _fieldName;
+	std::string _ownershipName;
+};
+
+
+
+class Auto : public Field
+{
+public:
+	Auto(const std::string fieldname) : Field(fieldname) {};
+
+	virtual uint16_t getBuyingPrice() const override {
+		return AUTO_BUY_PRICE;
+	}
+	virtual uint16_t getRentalCost() const override {
+		return AUTO_RENTAL_CHARGE;
+	}
+};
+
+class Food : public Field
+{
+public:
+	Food(const std::string fieldname) : Field(fieldname) {};
+
+	virtual uint16_t getBuyingPrice() const override {
+		return FOOD_BUY_PRICE;
+	}
+	virtual uint16_t getRentalCost() const override {
+		return FOOD_RENTAL_CHARGE;
+	}
+};
+
+class Travel : public Field
+{
+public:
+	Travel(const std::string fieldname) : Field(fieldname) {};
+
+	virtual uint16_t getBuyingPrice() const override {
+		return TRAVEL_BUY_PRICE;
+	}
+	virtual uint16_t getRentalCost() const override {
+		return TRAVEL_RENTAL_CHARGE;
+	}
+};
+
+class Clother : public Field
+{
+public:
+	Clother(const std::string fieldname) : Field(fieldname) {};
+
+	virtual uint16_t getBuyingPrice() const override {
+		return CLOTHER_BUY_PRICE;
+	}
+	virtual uint16_t getRentalCost() const override {
+		return CLOTHER_RENTAL_CHARGE;
+	}
+};
+
+class Prison : public Field
+{
+public:
+	Prison(const std::string fieldname) : Field(fieldname) {};
+
+	virtual uint16_t getBuyingPrice() const override {
+		return PRISON_BUY_PRICE;
+	}
+	virtual uint16_t getRentalCost() const override {
+		return PRISON_RENTAL_CHARGE;
+	}
+};
+
+class Bank : public Field
+{
+public:
+	Bank(const std::string fieldname) : Field(fieldname) {};
+
+	virtual uint16_t getBuyingPrice() const override {
+		return BANK_BUY_PRICE;
+	}
+	virtual uint16_t getRentalCost() const override {
+		return BANK_RENTAL_CHARGE;
+	}
 };
 #endif

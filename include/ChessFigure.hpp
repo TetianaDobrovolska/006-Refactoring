389f8aee064abef1ee1897ad8eca03903db01a38
Lorenzo Mignanelli <LMignanelli@luxoft.com>
2021-03-31 02:01:00 +0200

    chess: convert figures to classes
    Signed-off-by: Lorenzo Mignanelli <LMignanelli@luxoft.com>

--- include/ChessFigure.hpp
+++ include/ChessFigure.hpp
@@ -5,31 +5,138 @@
 
 class ChessFigure
 {
-	
 public:
-	enum FigureType
-	{
-		ROOK,
-		KNIGHT,
-		BISHOP,
-		PAWN,
-		KING,
-		QUEEN
-	};
+    static constexpr char FIRST_COL = 'A';
+    static constexpr char LAST_COL = 'H';
+    static constexpr char FIRST_ROW = '1';
+    static constexpr char LAST_ROW = '8';
 
-        static constexpr char FIRST_COL = 'A';
-        static constexpr char LAST_COL = 'H';
-        static constexpr char FIRST_ROW = '1';
-        static constexpr char LAST_ROW = '8';
+    ChessFigure(const std::string& initialCoord);
+    virtual ~ChessFigure();
+    virtual bool Move(const std::string& nextCoord) = 0;
 
-	ChessFigure(FigureType type,std::string coord);
-	bool Move(std::string nextCoord);
-	virtual ~ChessFigure();
-private:
-        bool CheckMovementBoundary(const std::string& nextCoord);
+protected:
+    bool CheckMovementBoundary(const std::string& nextCoord);
 
-	FigureType type;
-	std::string currentCoord;
+    int GetColSteps(const std::string& nextCoord);
+    int GetRowSteps(const std::string& nextCoord);
+    bool ColChanged(const std::string& nextCoord);
+    bool RowChanged(const std::string& nextCoord);
+    bool OrthogonalMove(const std::string &nextCoord);
+    bool DiagonalMove(const std::string &nextCoord);
+
+    std::string currentCoord;
+};
+
+class Rook : public ChessFigure
+{
+public:
+    Rook(const std::string& initialCoord) : ChessFigure(initialCoord)
+    {
+    }
+
+    bool Move(const std::string& nextCoord) override
+    {
+        if (!CheckMovementBoundary(nextCoord))
+            return false;
+        return OrthogonalMove(nextCoord);
+    }
+};
+
+class Knight : public ChessFigure
+{
+public:
+    Knight(const std::string& initialCoord) : ChessFigure(initialCoord)
+    {
+    }
+
+    bool Move(const std::string& nextCoord) override
+    {
+        if (!CheckMovementBoundary(nextCoord))
+            return false;
+
+        int colSteps = GetColSteps(nextCoord);
+        int rawSteps = GetRowSteps(nextCoord);
+
+        return (colSteps == 1 && rawSteps == 2 || colSteps == 2 && rawSteps == 1);
+    }
+};
+
+class Bishop : public ChessFigure
+{
+public:
+    Bishop(const std::string& initialCoord) : ChessFigure(initialCoord)
+    {
+    }
+
+    bool Move(const std::string& nextCoord) override
+    {
+        if (!CheckMovementBoundary(nextCoord))
+            return false;
+
+        return DiagonalMove(nextCoord);
+    }
+};
+
+class King : public ChessFigure
+{
+public:
+    King(const std::string& initialCoord) : ChessFigure(initialCoord)
+    {
+    }
+
+    bool Move(const std::string& nextCoord) override
+    {
+        if (!CheckMovementBoundary(nextCoord))
+            return false;
+
+        int colSteps = GetColSteps(nextCoord);
+        int rawSteps = GetRowSteps(nextCoord);
+
+        return (colSteps <= 1 && rawSteps <= 1);
+    }
+};
+
+class Queen : public ChessFigure
+{
+public:
+    Queen(const std::string& initialCoord) : ChessFigure(initialCoord)
+    {
+    }
+
+    bool Move(const std::string& nextCoord) override
+    {
+        if (!CheckMovementBoundary(nextCoord))
+            return false;
+
+        bool colChanged = ColChanged(nextCoord);
+        bool rawChanged = RowChanged(nextCoord);
+
+        return (DiagonalMove(nextCoord) || !colChanged || !rawChanged);
+    }
+};
+
+class Pawn : public ChessFigure
+{
+public:
+    Pawn(const std::string& initialCoord) : ChessFigure(initialCoord)
+    {
+    }
+
+    bool Move(const std::string& nextCoord) override
+    {
+        if (!CheckMovementBoundary(nextCoord))
+            return false;
+
+        bool colChanged = ColChanged(nextCoord);
+        int rawSteps = GetRowSteps(nextCoord);
+
+        if (colChanged || nextCoord[1] <= currentCoord[1] ||
+           (rawSteps != 1 && (currentCoord[1] != '2' || nextCoord[1] != '4')))
+            return false;
+        else
+            return true;
+    }
 };
 
 #endif
--- src/ChessFigure.cpp
+++ src/ChessFigure.cpp
@@ -4,8 +4,7 @@
 
 using namespace std;
 
-ChessFigure::ChessFigure(ChessFigure::FigureType type, std::string coord) : type(type),
-currentCoord(coord)
+ChessFigure::ChessFigure(const string &initialCoord) : currentCoord(initialCoord)
 {
 }
 
@@ -16,44 +15,41 @@ ChessFigure::~ChessFigure()
 
 bool ChessFigure::CheckMovementBoundary(const std::string& nextCoord)
 {
-    return (nextCoord[0] >= ChessFigure::FIRST_COL &&
-            nextCoord[0] <= ChessFigure::LAST_COL &&
-            nextCoord[1] >= ChessFigure::FIRST_ROW &&
-            nextCoord[1] <= ChessFigure::LAST_ROW);
+    bool validColumn = nextCoord[0] >= ChessFigure::FIRST_COL && nextCoord[0] <= ChessFigure::LAST_COL;
+    bool validRaw =    nextCoord[1] >= ChessFigure::FIRST_ROW && nextCoord[1] <= ChessFigure::LAST_ROW;
+
+    return validColumn && validRaw;
 }
 
-bool ChessFigure::Move(string nextCoord)
+int ChessFigure::GetColSteps(const string &nextCoord)
 {
-    if (!CheckMovementBoundary(nextCoord)) {
-        return false;
-    }
+    return abs(nextCoord[0] - currentCoord[0]);
+}
 
-    int colSteps = abs(nextCoord[0] - currentCoord[0]);
-    int rawSteps = abs(nextCoord[1] - currentCoord[1]);
-    bool colChanged = colSteps != 0;
-    bool rawChanged = rawSteps != 0;
-    bool orthogonalMove = colChanged && !rawChanged || !colChanged && rawChanged;
-    bool diagonalMove = colSteps == rawSteps;
+int ChessFigure::GetRowSteps(const string &nextCoord)
+{
+    return abs(nextCoord[1] - currentCoord[1]);
+}
 
-    switch (type) {
-    case PAWN:
-        if (colChanged || nextCoord[1] <= currentCoord[1] ||
-           (rawSteps != 1 && (currentCoord[1] != '2' || nextCoord[1] != '4')))
-            return false;
-        else
-            return true;
+bool ChessFigure::ColChanged(const string &nextCoord)
+{
+    return GetColSteps(nextCoord) != 0;
+}
 
-    case ROOK:
-        return orthogonalMove;
-    case KNIGHT:
-        return (colSteps == 1 && rawSteps == 2 || colSteps == 2 && rawSteps == 1);
-    case BISHOP:
-        return diagonalMove;
-    case KING:
-        return (colSteps <= 1 && rawSteps <= 1);
-    case QUEEN:
-        return (diagonalMove || !colChanged || !rawChanged);
-    default:
-        return false;
-    }
+bool ChessFigure::RowChanged(const string &nextCoord)
+{
+    return GetRowSteps(nextCoord) != 0;
+}
+
+bool ChessFigure::OrthogonalMove(const string &nextCoord)
+{
+    bool colChanged = ColChanged(nextCoord);
+    bool rawChanged = RowChanged(nextCoord);
+
+    return colChanged && !rawChanged || !colChanged && rawChanged;
+}
+
+bool ChessFigure::DiagonalMove(const string &nextCoord)
+{
+    return GetColSteps(nextCoord) == GetRowSteps(nextCoord);
 }

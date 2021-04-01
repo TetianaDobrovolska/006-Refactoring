3f6c0d9d675917e98337c78264e3fe286441e80b
Dim-Sal <dimitris_fme@yahoo.com>
2021-04-01 14:48:11 +0200

    Fix typos and logic
      * Fix typos in output texts
      * Fix game logic, to produce expected outcome

--- src/tic_tac_toe.cpp
+++ src/tic_tac_toe.cpp
@@ -3,6 +3,8 @@
 #include <clocale>
 #include <stdio.h>
 #include <stdlib.h>
+#include <regex>
+
 using namespace std;
 
 char win = '-'; 
@@ -10,14 +12,14 @@ char PlayerName1[80], PlayerName2[80];
 char cells[9] = { '-','-','-','-','-','-','-','-','-' };
 
 void show_cells() {
-    system("cls");
+    system("clear");
 
-    cout << "Cell numbers: \n";
+    cout << "Cell numbers: \n" << endl;
     cout << "-" << 1 << "-" << "|" << "-" << 2 << "-" << "|" << "-" << 3 << "-" << endl;
     cout << "-" << 4 << "-" << "|" << "-" << 5 << "-" << "|" << "-" << 6 << "-" << endl;
     cout << "-" << 7 << "-" << "|" << "-" << 8 << "-" << "|" << "-" << 9 << "-" << endl;
 
-    cout << "Current situation (--- empty):\n" << endl;
+    cout << "\nCurrent situation (--- empty):\n" << endl;
     cout << "-" << cells[0] << "-" << '|' << "-" << cells[1] << "-" << '|' << "-" << cells[2] << "-" << endl;
     cout << "-" << cells[3] << "-" << '|' << "-" << cells[4] << "-" << '|' << "-" << cells[5] << "-" << endl;
     cout << "-" << cells[6] << "-" << '|' << "-" << cells[7] << "-" << '|' << "-" << cells[8] << "-" << "\n" << endl;
@@ -27,47 +29,58 @@ void show_cells() {
 void make_move(int num) {
     if (num == 1) cout << PlayerName1;
     else cout << PlayerName2;
-    int cell;
-    cout << ",enter cell number, make your move:";
+    std::string cell;
+    cout << ", enter cell number, make your move: ";
     cin >> cell;
-  
 
-       
-    while (cell > 9 || cell < 1 || cells[cell - 1] == 'O' || cells[cell - 1] == 'X') {
-        cout << "Enter the number of the correct (1-9) or empty (---) cells to make a move:";
+    std::regex rx("[1-9]");
+    while ((!std::regex_match(cell, rx)) || (cells[stoi(cell) - 1] == 'O') || (cells[stoi(cell) - 1] == 'X')) {
+        cout << "Empty shells are marked with (---). \n"
+                "Enter the number (1-9) of an empty cell to make a move: ";
         cin >> cell;
         cout << "\n";
     }
     
-    if (num == 1) cells[cell - 1] = 'X';
-    else cells[cell - 1] = 'O';
+    if (num == 1) cells[stoi(cell) - 1] = 'X';
+    else cells[stoi(cell) - 1] = 'O';
 }
 
 char check()
 {
     for (int i = 0; i < 3; i++) 
-        if (cells[i * 3] == cells[i * 3 + 1] && cells[i * 3 + 1] == cells[i * 3 + 2])
-            return cells[i]; 
-        else if (cells[i] == cells[i + 3] && cells[i + 3] == cells[i + 6]) 
+        if ((cells[i * 3] == cells[i * 3 + 1] && cells[i * 3 + 1] == cells[i * 3 + 2]) && cells[i * 3] != '-')
+        {
+            cout << "Row win" << i+1 << ": " << cells[i * 3] << " " << cells[i * 3 + 1] << " " << cells[i * 3 + 2] << endl;
+            return cells[i * 3];
+        }
+        else if ((cells[i] == cells[i + 3] && cells[i + 3] == cells[i + 6]) && cells[i * 3] != '-')
+        {
+            cout << "Col win: " << i+1 << ": " << cells[i] << " " << cells[i + 3] << " " << cells[i + 6] << endl;
             return cells[i];
-        else if ((cells[2] == cells[4] && cells[4] == cells[6]) || (cells[0] == cells[4] && cells[4] == cells[8]))
-            return cells[i]; 
+        }
+        else if (((cells[2] == cells[4] && cells[4] == cells[6]) || (cells[0] == cells[4] && cells[4] == cells[8])) && cells[i * 3] != '-')
+        {
+            cout << "Diagonal win: " << cells[2] << " " << cells[4] << " " << cells[6] << endl;
+            return cells[4];
+        }
     return '-'; 
 }
 
 void result() {
     if (win == 'X')
-        cout << PlayerName1 << "you won congratulations " << PlayerName2 << " you lose..." << endl;
-    else if (win == 'O') cout << PlayerName2 << "you won congratulations  " << PlayerName1 << " you lose..." << endl;
+        cout << PlayerName1 << ", you won. Congratulations! " << PlayerName2 << ", you lose..." << endl;
+    else if (win == 'O') cout << PlayerName2 << ", you won. Congratulations! " << PlayerName1 << ", you lose..." << endl;
 
 }
 
 int main(int argc, char** argv) {
+    system("clear");
+
     do {
         cout << "Enter the name of the first player: ";
         std::cin >> PlayerName1;
 
-        cout << "Enter the name of the seecond player: ";
+        cout << "Enter the name of the second player: ";
         std::cin >> PlayerName2;
     } while (!strcmp(PlayerName1, PlayerName2));
     

3e8af2643061923e58e7c7e279bbb489e7f59247
Igor Kovalenko <iikovalenko@luxoft.com>
2021-04-19 23:46:09 +0300

    Step1-Tests

--- test/main_tests/test.cpp
+++ test/main_tests/test.cpp
@@ -2,14 +2,63 @@
 #include "string_calc.hpp"
 #include <iostream>
 
-TEST(CalculatorTest, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
-}
+ TEST(CalculatorTest, ZeroParams) {
+    StringCalc c;
+    int actual = c.Add("");
+    ASSERT_EQ(actual, 0);
+ }
 
-// TEST(CalculatorTest, SampleTest) {
-// 	StringCalc c;
-// 	int actual = c.Add("2,2");
-// 	ASSERT_EQ(actual, 4);
-// }
+ TEST(CalculatorTest, LowerBoundOneParam){
+     StringCalc c;
+     int actual = c.Add("0");
+     ASSERT_EQ(actual, 0);
+ }
+
+ TEST(CalculatorTest, UpperBoundOneParam){
+     StringCalc c;
+     int actual = c.Add("1000000");
+     ASSERT_EQ(actual, 1'000'000);
+ }
+
+ TEST(CalculatorTest, LowerBoundTwoParams){
+     StringCalc c;
+     int actual = c.Add("0,0");
+     ASSERT_EQ(actual, 0);
+ }
+
+ TEST(CalculatorTest, UpperBoundTwoParams){
+     StringCalc c;
+     int actual = c.Add("1000000,1000000");
+     ASSERT_EQ(actual, 2'000'000);
+ }
+
+ TEST(CalculatorTest, AllowedOnlyPositive){
+     StringCalc c;
+     int actual = c.Add("-2");
+     ASSERT_EQ(actual, -1);
+ }
+
+ TEST(CalculatorTest, NotANumber){
+     StringCalc c;
+     int actual = c.Add("a");
+     ASSERT_EQ(actual, -1);
+ }
+
+ TEST(CalculatorTest, WrongSeparator){
+     StringCalc c;
+     int actual = c.Add("1 2");
+     ASSERT_EQ(actual, -1);
+ }
+
+ TEST(CalculatorTest, MoreThanTwoParams){
+     StringCalc c;
+     int actual = c.Add("1,2,3,4");
+     ASSERT_EQ(actual, -1);
+ }
+
+ TEST(CalculatorTest, RandomTest){
+     StringCalc c;
+     int actual = c.Add("100,20,3");
+     ASSERT_EQ(actual, 123);
+ }
 

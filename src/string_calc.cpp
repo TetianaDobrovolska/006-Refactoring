7b2ecc5552889095bb048bb9b37f5a8dd73bdad5
km591192 <km591192@bk.ru>
2021-04-19 19:52:42 +0300

    TDD Step1

--- src/string_calc.cpp
+++ src/string_calc.cpp
@@ -1,20 +1,52 @@
 #include "string_calc.hpp"
+
 #include <vector>
+
 #include <algorithm>
+
+
 
 using namespace std;
 
+
+
 StringCalc::StringCalc()
+
 {
+
 }
+
+
+
 
 
 StringCalc::~StringCalc()
+
 {
+
 }
 
-
 int StringCalc::Add(string numbers)
+
 {
-	return 0;//Not Implemented yet
+
+	string delimiter = ",";
+
+	int pos = 0;
+	int num = 0;
+	int sum = 0;
+
+	do {
+		pos = numbers.find(delimiter);
+		num = atoi(numbers.substr(0, pos).c_str());
+		if (num < 0) {
+			sum = -1; 
+			break;
+		}
+		sum += num;
+		numbers.erase(0, pos + delimiter.length());
+	} while (pos != string::npos);	
+
+	return sum;
+
 }
--- test/main_tests/test.cpp
+++ test/main_tests/test.cpp
@@ -1,15 +1,80 @@
 #include "gtest/gtest.h"
+
 #include "string_calc.hpp"
+
 #include <iostream>
 
+
+
 TEST(CalculatorTest, TestName) {
+
   EXPECT_EQ(1, 1);
+
   EXPECT_TRUE(true);
+
 }
 
-// TEST(CalculatorTest, SampleTest) {
-// 	StringCalc c;
-// 	int actual = c.Add("2,2");
-// 	ASSERT_EQ(actual, 4);
-// }
+
+
+TEST(CalculatorTest, SampleTest) {
+
+ 	StringCalc c;
+
+ 	int actual = c.Add("1,2");
+
+ 	ASSERT_EQ(actual, 3);
+
+ }
+
+
+
+
+TEST(CalculatorTest, EmptyString) {
+
+ 	StringCalc c;
+
+ 	int actual = c.Add("");
+
+ 	ASSERT_EQ(actual, 0);
+
+ }
+
+
+
+
+TEST(CalculatorTest, Symbol0) {
+
+ 	StringCalc c;
+
+ 	int actual = c.Add("0");
+
+ 	ASSERT_EQ(actual, 0);
+
+ }
+
+
+
+
+TEST(CalculatorTest, Symbol1) {
+
+ 	StringCalc c;
+
+ 	int actual = c.Add("1");
+
+ 	ASSERT_EQ(actual, 1);
+
+ }
+
+
+
+
+TEST(CalculatorTest, StringSub) {
+
+ 	StringCalc c;
+
+ 	int actual = c.Add("2,-1");
+
+ 	ASSERT_EQ(actual, -1);
+
+ }
 

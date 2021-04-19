03b68a5c6ecf074c0878cf57a392912307e95377
Zhan Poplavskyi <gan.popl@gmail.com>
2021-04-19 18:41:04 +0300

    Add a few tests with simple behavior

--- test/main_tests/test.cpp
+++ test/main_tests/test.cpp
@@ -2,14 +2,33 @@
 #include "string_calc.hpp"
 #include <iostream>
 
-TEST(CalculatorTest, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+TEST(CalculatorTest, AddZeroNumbers) {
+  StringCalc c;
+  int actual = c.Add("");
+  ASSERT_EQ(actual, 0);
 }
 
-// TEST(CalculatorTest, SampleTest) {
-// 	StringCalc c;
-// 	int actual = c.Add("2,2");
-// 	ASSERT_EQ(actual, 4);
-// }
+TEST(CalculatorTest, AddOneNumber) {
+	StringCalc c;
+	int actual = c.Add("1");
+	ASSERT_EQ(actual, 1);
+}
+
+TEST(CalculatorTest, AddTwoNumbers) {
+	StringCalc c;
+	int actual = c.Add("1,2");
+	ASSERT_EQ(actual, 3);
+}
+
+TEST(CalculatorTest, AddNegativeNumber) {
+	StringCalc c;
+	int actual = c.Add("-666");
+	ASSERT_EQ(actual, -1);
+}
+
+TEST(CalculatorTest, AddNotANumber) {
+	StringCalc c;
+	int actual = c.Add("k");
+	ASSERT_EQ(actual, -1);
+}
 

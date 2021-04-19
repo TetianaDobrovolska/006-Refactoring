9c1f63c100d58a9c90c1f8128a5eb90d845521c9
Kryvonos <MKryvonos@kiev.luxoft.com>
2021-04-19 23:31:38 +0300

    calc step1

--- src/string_calc.cpp
+++ src/string_calc.cpp
@@ -1,6 +1,7 @@
 #include "string_calc.hpp"
 #include <vector>
 #include <algorithm>
+#include <sstream>
 
 using namespace std;
 
@@ -13,8 +14,40 @@ StringCalc::~StringCalc()
 {
 }
 
+int convertToInt(string string) {
+	int number = 0;
+
+	for(auto ch : string) {
+		int num = ch - '0';
+		if((num >= 0)and(num <=9))
+			number = (number * 10) + num;
+		else
+			return -1;
+	}
+	return number;
+}
 
 int StringCalc::Add(string numbers)
 {
-	return 0;//Not Implemented yet
+	std::vector<std::string> vect;
+	std::stringstream ss(numbers);
+	int result = 0;
+	
+    std::string item;
+	while (std::getline(ss, item, ',')) {
+		vect.push_back(item);
+	}
+	
+	if(vect.size() > 2)
+		result = -1;
+
+    for (std::size_t i = 0; i < vect.size() and result != -1; i++) {
+		int num = convertToInt(vect[i]);
+		if(num >=0)
+			result += num;
+		else 
+			result = -1;
+	}
+        
+	return result;
 }
--- test/main_tests/test.cpp
+++ test/main_tests/test.cpp
@@ -7,9 +7,73 @@ TEST(CalculatorTest, TestName) {
   EXPECT_TRUE(true);
 }
 
-// TEST(CalculatorTest, SampleTest) {
-// 	StringCalc c;
-// 	int actual = c.Add("2,2");
-// 	ASSERT_EQ(actual, 4);
-// }
+TEST(CalculatorTest, SampleTest) {
+	StringCalc c;
+ 	int actual = c.Add("2,2");
+ 	ASSERT_EQ(actual, 4);
+}
 
+TEST(CalculatorTest, Sample2SymbolsTest) {
+	StringCalc c;
+ 	int actual = c.Add("10,2");
+ 	ASSERT_EQ(actual, 12);
+}
+
+TEST(CalculatorTest, SampleDigAndCharTest) {
+	StringCalc c;
+ 	int actual = c.Add("1a,2");
+ 	ASSERT_EQ(actual, -1);
+}
+ 
+TEST(CalculatorTest, SampleOneOp1Test) {
+	StringCalc c;
+ 	int actual = c.Add("0");
+ 	ASSERT_EQ(actual, 0);
+}
+
+TEST(CalculatorTest, SampleOneOp2Test) {
+	StringCalc c;
+ 	int actual = c.Add("1");
+ 	ASSERT_EQ(actual, 1);
+}
+
+TEST(CalculatorTest, SampleEmptyLineTest) {
+	StringCalc c;
+ 	int actual = c.Add("");
+ 	ASSERT_EQ(actual, 0);
+}
+
+TEST(CalculatorTest, SampleOneNegativeOpTest) {
+	StringCalc c;
+ 	int actual = c.Add("-1");
+ 	ASSERT_EQ(actual, -1);
+}
+TEST(CalculatorTest, SampleFirstNegativeOpTest) {
+	StringCalc c;
+ 	int actual = c.Add("-1,1");
+ 	ASSERT_EQ(actual, -1);
+}
+
+TEST(CalculatorTest, SampleSecondNegativeOpTest) {
+	StringCalc c;
+ 	int actual = c.Add("1,-1");
+ 	ASSERT_EQ(actual, -1);
+}
+
+TEST(CalculatorTest, SampleNotNumbers1Test) {
+	StringCalc c;
+ 	int actual = c.Add("a,b");
+ 	ASSERT_EQ(actual, -1);
+}
+
+TEST(CalculatorTest, SampleNotNumbers2Test) {
+	StringCalc c;
+ 	int actual = c.Add("a");
+ 	ASSERT_EQ(actual, -1);
+}
+
+TEST(CalculatorTest, SampleMoreThanTwoOpsTest) {
+	StringCalc c;
+ 	int actual = c.Add("1,2,3");
+ 	ASSERT_EQ(actual, -1);
+}

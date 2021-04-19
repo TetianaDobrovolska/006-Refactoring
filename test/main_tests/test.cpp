f552340045a15ae21b7177627d3593d6795e0d20
km591192 <km591192@bk.ru>
2021-04-19 20:22:05 +0300

    TDD Step2

--- test/main_tests/test.cpp
+++ test/main_tests/test.cpp
@@ -68,11 +68,37 @@ TEST(CalculatorTest, Symbol1) {
 
 
 
-TEST(CalculatorTest, StringSub) {
+TEST(CalculatorTest, StringWrongArg) {
 
  	StringCalc c;
 
  	int actual = c.Add("2,-1");
+
+ 	ASSERT_EQ(actual, -1);
+
+ }
+
+
+
+
+TEST(CalculatorTest, SumFiveEL) {
+
+ 	StringCalc c;
+
+ 	int actual = c.Add("5,100,20,5,1");
+
+ 	ASSERT_EQ(actual, 131);
+
+ }
+
+
+
+
+TEST(CalculatorTest, SumFiveElWrongArg) {
+
+ 	StringCalc c;
+
+ 	int actual = c.Add("5,100,-10,5,1");
 
  	ASSERT_EQ(actual, -1);
 

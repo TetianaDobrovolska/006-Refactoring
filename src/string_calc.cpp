8be09c9b2dac1adbb5aac4b6576691ed66be60f6
gewioO <work.gewi@gmail.com>
2021-04-20 08:43:15 +0300

    Add sum function

--- src/string_calc.cpp
+++ src/string_calc.cpp
@@ -1,6 +1,8 @@
 #include "string_calc.hpp"
 #include <vector>
 #include <algorithm>
+#include <regex>
+#include <numeric>
 
 using namespace std;
 
@@ -16,5 +18,18 @@ StringCalc::~StringCalc()
 
 int StringCalc::Add(string numbers)
 {
-	return 0;//Not Implemented yet
+	vector<int> vect;
+	static const regex regular("\\d+");
+	int i = 0;
+	while(i != -1) {
+		string temp = numbers.substr(i, numbers.find(",", i+1));
+    	regex_token_iterator<string::iterator> num( temp.begin(), temp.end(), regular);
+    	string t = *num;
+    	int elem = atoi( t.c_str() );
+		i = numbers.find(",", i+1);
+		vect.push_back(elem);
+	}
+	int sum_of_elems = std::accumulate(vect.begin(), vect.end(), 0);
+	return sum_of_elems;
+	
 }

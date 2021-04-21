46879ad11920c0703681a048c4fd86d902214cbb
Denis Plotnikov <virtuallyworks@gmail.com>
2021-04-21 19:56:26 +0300

    Step 1

--- src/string_calc.cpp
+++ src/string_calc.cpp
@@ -1,8 +1,11 @@
 #include "string_calc.hpp"
 #include <vector>
 #include <algorithm>
+#include <string>
 
 using namespace std;
+
+bool ParseInputString(const string&, vector<int>&);
 
 StringCalc::StringCalc()
 {
@@ -14,7 +17,72 @@ StringCalc::~StringCalc()
 }
 
 
-int StringCalc::Add(string numbers)
+int StringCalc::Add(const string& numbers)
 {
-	return 0;//Not Implemented yet
+	vector<int> arguments;
+
+	bool correct_arguments = ParseInputString(numbers, arguments);
+
+	if (correct_arguments == 0)
+	{
+		return -1;
+	}
+
+	int sum = 0;
+	for (const int& argument : arguments)
+	{
+		sum += argument;
+	}
+	return sum;
+}
+
+
+bool ParseInputString(const string& source_string, vector<int>& arguments) {
+	size_t string_size = source_string.size();
+
+	if (string_size == 0)
+	{
+		arguments.push_back(0);
+		return 1;
+	}
+
+	string part_of_string;
+	int position = 0;
+	int divider_count = 0;
+
+	for (const auto& later : source_string) {
+		if (later != ',')
+		{
+			if (!isdigit(later))
+			{
+				return 0;
+			}
+
+			part_of_string += later;
+
+			if (position == string_size -1)
+			{
+				arguments.push_back(stoi(part_of_string));
+				return 1;
+			}
+		}
+		else
+		{
+			divider_count++;
+
+			if (arguments.size() < 2 && divider_count < 2 && !part_of_string.empty())
+			{
+				arguments.push_back(stoi(part_of_string));
+				part_of_string = "";
+			}
+			else
+			{
+				return 0;
+			}
+		}
+		position++;
+	}
+
+	return 0;
+
 }
--- test/main_tests/test.cpp
+++ test/main_tests/test.cpp
@@ -7,9 +7,98 @@ TEST(CalculatorTest, TestName) {
   EXPECT_TRUE(true);
 }
 
-// TEST(CalculatorTest, SampleTest) {
-// 	StringCalc c;
-// 	int actual = c.Add("2,2");
-// 	ASSERT_EQ(actual, 4);
-// }
+TEST(CalculatorTest, Test1) {
+ 	StringCalc c;
+ 	int actual = c.Add("3,4");
+ 	ASSERT_EQ(actual, 7);
+}
 
+TEST(CalculatorTest, Test2) {
+ 	StringCalc c;
+ 	int actual = c.Add("");
+ 	ASSERT_EQ(actual, 0);
+}
+
+TEST(CalculatorTest, Test3) {
+ 	StringCalc c;
+ 	int actual = c.Add("0");
+ 	ASSERT_EQ(actual, 0);
+}
+
+TEST(CalculatorTest, Test4) {
+ 	StringCalc c;
+ 	int actual = c.Add("3,4,");
+ 	ASSERT_EQ(actual, -1);
+}
+
+TEST(CalculatorTest, Test5) {
+ 	StringCalc c;
+ 	int actual = c.Add("3,,4");
+ 	ASSERT_EQ(actual, -1);
+}
+
+TEST(CalculatorTest, Test6) {
+ 	StringCalc c;
+ 	int actual = c.Add(",3");
+ 	ASSERT_EQ(actual, -1);
+}
+
+TEST(CalculatorTest, Test7) {
+ 	StringCalc c;
+ 	int actual = c.Add(",");
+ 	ASSERT_EQ(actual, -1);
+}
+
+TEST(CalculatorTest, Test8) {
+ 	StringCalc c;
+ 	int actual = c.Add("7,");
+ 	ASSERT_EQ(actual, -1);
+}
+
+TEST(CalculatorTest, Test9) {
+ 	StringCalc c;
+ 	int actual = c.Add("34");
+ 	ASSERT_EQ(actual, 34);
+}
+
+TEST(CalculatorTest, Test10) {
+ 	StringCalc c;
+ 	int actual = c.Add("1,20");
+ 	ASSERT_EQ(actual, 21);
+}
+
+TEST(CalculatorTest, Test11) {
+ 	StringCalc c;
+ 	int actual = c.Add("17,23");
+ 	ASSERT_EQ(actual, 40);
+}
+
+TEST(CalculatorTest, Test12) {
+ 	StringCalc c;
+ 	int actual = c.Add("a");
+ 	ASSERT_EQ(actual, -1);
+}
+
+TEST(CalculatorTest, Test13) {
+ 	StringCalc c;
+ 	int actual = c.Add("1a");
+ 	ASSERT_EQ(actual, -1);
+}
+
+TEST(CalculatorTest, Test14) {
+ 	StringCalc c;
+ 	int actual = c.Add("5a,11");
+ 	ASSERT_EQ(actual, -1);
+}
+
+TEST(CalculatorTest, Test15) {
+ 	StringCalc c;
+ 	int actual = c.Add("12,c");
+ 	ASSERT_EQ(actual, -1);
+}
+
+TEST(CalculatorTest, Test16) {
+ 	StringCalc c;
+ 	int actual = c.Add("9,5a");
+ 	ASSERT_EQ(actual, -1);
+}

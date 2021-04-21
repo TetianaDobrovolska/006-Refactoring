85924ac96384dfc467796acf984272803e7ad165
Igor Kovalenko <iikovalenko@luxoft.com>
2021-04-21 22:38:59 +0300

    Refactoring-2-Tests

--- test/main_tests/test.cpp
+++ test/main_tests/test.cpp
@@ -3,140 +3,121 @@
 #include <iostream>
 #include <stdexcept>
 
- TEST(CalculatorTest, ZeroParams) {
+class CalculatorTest : public ::testing::Test {
+protected:
     StringCalc c;
+};
+
+ TEST_F(CalculatorTest, ZeroParams) {
     int actual = c.Add("");
     ASSERT_EQ(actual, 0);
  }
 
- TEST(CalculatorTest, LowerBoundOneParam){
-     StringCalc c;
+ TEST_F(CalculatorTest, LowerBoundOneParam){
      int actual = c.Add("0");
      ASSERT_EQ(actual, 0);
  }
 
- TEST(CalculatorTest, UpperBoundOneParam){
-     StringCalc c;
+ TEST_F(CalculatorTest, UpperBoundOneParam){
      int actual = c.Add("1000");
      ASSERT_EQ(actual, 1'000);
  }
 
- TEST(CalculatorTest, OutUpperBoundOneParam){
-     StringCalc c;
+ TEST_F(CalculatorTest, OutUpperBoundOneParam){
      int actual = c.Add("1001");
      ASSERT_EQ(actual, 0);
  }
 
- TEST(CalculatorTest, LowerBoundTwoParams){
-     StringCalc c;
+ TEST_F(CalculatorTest, LowerBoundTwoParams){
      int actual = c.Add("0,0");
      ASSERT_EQ(actual, 0);
  }
 
- TEST(CalculatorTest, UpperBoundTwoParams){
-     StringCalc c;
+ TEST_F(CalculatorTest, UpperBoundTwoParams){
      int actual = c.Add("1000,1000");
      ASSERT_EQ(actual, 2'000);
  }
 
- TEST(CalculatorTest, OutUpperBoundTwoParams){
-     StringCalc c;
+ TEST_F(CalculatorTest, OutUpperBoundTwoParams){
      int actual = c.Add("1001,1000");
      ASSERT_EQ(actual, 1'000);
  }
 
- TEST(CalculatorTest, AllowedOnlyPositive){
-     StringCalc c;
+ TEST_F(CalculatorTest, AllowedOnlyPositive){
      ASSERT_THROW(c.Add("-2"), std::invalid_argument);
  }
 
- TEST(CalculatorTest, NotANumber){
-     StringCalc c;
+ TEST_F(CalculatorTest, NotANumber){
      ASSERT_THROW(c.Add("a"), std::invalid_argument);
  }
 
- TEST(CalculatorTest, WrongSeparator){
-     StringCalc c;
+ TEST_F(CalculatorTest, WrongSeparator){
      ASSERT_THROW(c.Add("1-2"), std::invalid_argument);
  }
 
- TEST(CalculatorTest, MixedSeparators){
-     StringCalc c;
+ TEST_F(CalculatorTest, MixedSeparators){
      int actual = c.Add("1,2\n3\n4");
      ASSERT_EQ(actual, 10);
  }
 
- TEST(CalculatorTest, SpecialSeparatorsValid1){
-     StringCalc c;
+ TEST_F(CalculatorTest, SpecialSeparatorsValid1){
      int actual = c.Add("//;\n1;2");
      ASSERT_EQ(actual, 3);
  }
 
- TEST(CalculatorTest, SpecialSeparatorsValid2){
-     StringCalc c;
+ TEST_F(CalculatorTest, SpecialSeparatorsValid2){
      int actual = c.Add("//;\n1;2;7");
      ASSERT_EQ(actual, 10);
  }
 
- TEST(CalculatorTest, SpecialSeparatorsValid3){
-     StringCalc c;
+ TEST_F(CalculatorTest, SpecialSeparatorsValid3){
      int actual = c.Add("//[***]\n1***2***7");
      ASSERT_EQ(actual, 10);
  }
 
- TEST(CalculatorTest, SpecialSeparatorsValid4){
-     StringCalc c;
+ TEST_F(CalculatorTest, SpecialSeparatorsValid4){
      int actual = c.Add("//[;ab]\n1;ab2;ab7");
      ASSERT_EQ(actual, 10);
  }
 
- TEST(CalculatorTest, SpecialSeparatorsNegative1){
-     StringCalc c;
+ TEST_F(CalculatorTest, SpecialSeparatorsNegative1){
      int actual = c.Add("//;\n1;2,7");
      ASSERT_EQ(actual, 10);
  }
 
- TEST(CalculatorTest, SpecialSeparatorsNegative2){
-     StringCalc c;
+ TEST_F(CalculatorTest, SpecialSeparatorsNegative2){
      ASSERT_THROW(c.Add("/;\n1;2;7"), std::invalid_argument);
  }
 
- TEST(CalculatorTest, SpecialSeparatorsNegative3){
-     StringCalc c;
+ TEST_F(CalculatorTest, SpecialSeparatorsNegative3){
      ASSERT_THROW(c.Add("//;\n\n1;2;7"), std::invalid_argument);
  }
 
- TEST(CalculatorTest, SpecialSeparatorsNegative4){
-     StringCalc c;
+ TEST_F(CalculatorTest, SpecialSeparatorsNegative4){
      ASSERT_THROW(c.Add("//[***\n1***2***7"), std::invalid_argument);
  }
 
- TEST(CalculatorTest, MoreThanTwoParams){
-     StringCalc c;
+ TEST_F(CalculatorTest, MoreThanTwoParams){
      int actual = c.Add("1,2,3,4");
      ASSERT_EQ(actual, 10);
  }
 
- TEST(CalculatorTest, RandomTest1){
-     StringCalc c;
+ TEST_F(CalculatorTest, RandomTest1){
      int actual = c.Add("100,20,3");
      ASSERT_EQ(actual, 123);
  }
 
- TEST(CalculatorTest, RandomTest2){
-     StringCalc c;
+ TEST_F(CalculatorTest, RandomTest2){
      int actual = c.Add("123,23,1,2,3");
      ASSERT_EQ(actual, 152);
  }
 
- TEST(CalculatorTest, RandomTest3){
-     StringCalc c;
+ TEST_F(CalculatorTest, RandomTest3){
      int actual = c.Add("123,23,1,2,3\n10");
      ASSERT_EQ(actual, 162);
  }
 
- TEST(CalculatorTest, RandomTest4){
-     StringCalc c;
+ TEST_F(CalculatorTest, RandomTest4){
      int actual = c.Add("123,23,1,2,3\n10,1001");
      ASSERT_EQ(actual, 162);
  }

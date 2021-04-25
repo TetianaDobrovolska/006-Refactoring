4a1a74d31be7ff3dd49654327959b5bb2bb532f6
Tetiana Dobrovolska <TDobrovolska@luxoft.com>
2021-04-25 22:09:42 +0300

    DataValue. Add Player class

---
+++ include/player.h
@@ -0,0 +1,20 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include <string>
+
+class Player
+{
+public:
+    Player(const std::string& name, const int& balance);
+
+    const int& getBalance() const;
+    const std::string& getName() const;
+
+    void calcBalance(const int& delta);
+private:
+    std::string Name;
+    int Balance;
+};
+
+#endif // PLAYER_H
---
+++ src/player.cpp
@@ -0,0 +1,23 @@
+#include "player.h"
+
+Player::Player(const std::string& name, const int& balance)
+    : Name(name)
+    , Balance(balance)
+{
+
+}
+
+const int& Player::getBalance() const
+{
+    return Balance;
+}
+
+void Player::calcBalance(const int &delta)
+{
+    Balance += delta;
+}
+
+const std::string &Player::getName() const
+{
+    return Name;
+}

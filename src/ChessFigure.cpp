bab93df04e0cb0a1dabfceb2bcc1f2bd38e1ae5c
Tetiana Dobrovolska <TDobrovolska@kiev.luxoft.com>
2021-04-27 14:19:52 +0300

    Remove unused code

--- src/ChessFigure.cpp
+++ src/ChessFigure.cpp
@@ -4,12 +4,17 @@
 
 namespace {
 
+constexpr int kFirstLetter = 'A';
+constexpr int kLastLetter = 'H';
+constexpr int kFirstDigit = '1';
+constexpr int kLastDigit = '8';
+
 bool isCoordValid(const std::string& coord)
 {
-  return coord[0] >= 'A' &&
-      coord[0] <= 'H' &&
-      coord[1] >= '1' &&
-      coord[1] <= '8';
+  return coord[0] >= kFirstLetter &&
+      coord[0] <= kLastLetter &&
+      coord[1] >= kFirstDigit &&
+      coord[1] <= kLastDigit;
 }
 
 }  //namespace
@@ -28,104 +33,4 @@ bool ChessFigure::Move(const std::string& nextCoord)
   if (!isCoordValid(nextCoord))
     return false;
   return type->Move(currentCoord, nextCoord) ? true : false;
-	/*if (type == PAWN)
-	{
-
-		if (nextCoord[0] != currentCoord[0] || nextCoord[1] <= currentCoord[1] || (nextCoord[1] - currentCoord[1] != 1 && (currentCoord[1] != '2' || nextCoord[1] != '4')))
-			return false;
-		else
-			return true;
-	}
-
-	else if (type == ROOK)
-	{
-		if ((nextCoord[0] != currentCoord[0]) && (nextCoord[1] != currentCoord[1]) || ((nextCoord[0] == currentCoord[0]) && (nextCoord[1] == currentCoord[1])))
-			return false;
-		else
-			return true;
-	}
-
-	else if (type == KNIGHT)
-	{
-		int dx, dy;
-		dx = abs(nextCoord[0] - currentCoord[0]);
-		dy = abs(nextCoord[1] - currentCoord[1]);
-		if (!(abs(nextCoord[0] - currentCoord[0]) == 1 && abs(nextCoord[1] - currentCoord[1]) == 2 || abs(nextCoord[0] - currentCoord[0]) == 2 && abs(nextCoord[1] - currentCoord[1]) == 1))
-			return false;
-		else
-			return true;
-	}
-
-	else if (type == BISHOP)
-	{
-		if (!(abs(nextCoord[0] - currentCoord[0]) == abs(nextCoord[1] - currentCoord[1])))
-			return false;
-		else
-			return true;
-	}
-
-	else if (type == KING)
-	{
-		if (!(abs(nextCoord[0] - currentCoord[0]) <= 1 && abs(nextCoord[1] - currentCoord[1]) <= 1))
-			return false;
-		else
-			return true;
-	}
-
-	else if (type == QUEEN)
-	{
-		if (!(abs(nextCoord[0] - currentCoord[0]) == abs(nextCoord[1] - currentCoord[1]) || nextCoord[0] == currentCoord[0] || nextCoord[1] == currentCoord[1]))
-			return false;
-		else
-			return true;
-	}
-	else
-		return false;
-	*/
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
